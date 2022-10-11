99495b61fe14468e01d46d2f63eeb3c654b36467
Rarepug1st <99208797+Rarepug1st@users.noreply.github.com>
2022-10-11 14:29:53 -0400

    finished pin

--- src/DisplayMultiples/DisplayMultiples.java
+++ src/DisplayMultiples/DisplayMultiples.java
@@ -1,11 +1,29 @@
 package DisplayMultiples;
 
-import java.util.Scanner;
-
+import  java.util.Scanner;
 public class DisplayMultiples {
 
     public static void main(String args[]) {
+    	Scanner in = new Scanner(System.in);
+    	//int createdpin =  2006;
+    	//int userpin = in.nextInt();
+    System.out.println("welcome to rbc \n what is your username");
+    	String name = in.nextLine();
+        System.out.println("Welcome"+name+"! please enter your pin");
+        int Code = in.nextInt();
+        if (Code== Code){
+            System.out.println("Password correct");
+        }
+         else {  System.out.println("invaild password");
+         }
+        {
+    	
+    	}
+    	//createdpin = in.nextInt();
+    	//while (userPin!= createdpin)
        
     }
+
+    
 
 }
